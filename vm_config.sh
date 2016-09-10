526b8f9188b822dc83f6bc06f66385ac3e15ccb0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-09 17:21:59 -0700

    Update Node to v6

--- vm_config.sh
+++ vm_config.sh
@@ -3,7 +3,7 @@
 apt-get update -qqy
 apt-get install -qqy git
 apt-get install -qqy build-essential
-curl -sL https://deb.nodesource.com/setup_5.x | bash
+curl -sL https://deb.nodesource.com/setup_6.x | bash
 apt-get install -qqy nodejs
 
 npm install -g bower
